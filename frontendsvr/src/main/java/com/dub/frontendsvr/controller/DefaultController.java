85431f33357d264d6d5c890670e0c4d50a26ff31
dubersfeld <dominique42ubersfeld@gmail.com>
2018-02-22 08:53:52 +0100

    Logout added.

--- frontendsvr/src/main/java/com/dub/frontendsvr/controller/DefaultController.java
+++ frontendsvr/src/main/java/com/dub/frontendsvr/controller/DefaultController.java
@@ -1,9 +1,14 @@
 package com.dub.frontendsvr.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.session.SessionRegistry;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -41,7 +46,17 @@ public class DefaultController {
     
     	modelAndView.setViewName("login");
     	return modelAndView;
-}
+	}
 	
+	
+	@GetMapping(value="/logout")
+    public String logoutPage (HttpServletRequest request, HttpServletResponse response) {
+    	Authentication auth = SecurityContextHolder.getContext().getAuthentication();    	
+    	if (auth != null) { 
+            new SecurityContextLogoutHandler().logout(request, response, auth);
+    	}
 
+        return "redirect:/login?logout";
+	}
+	
 }
